3e244482c604da2f0dd54687c7627f0bace28877
olaniyi-emmanuel <emmanuel4sucess@outlook.com>
2023-11-24 12:25:15 -0800

    task

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,46 +1,39 @@
 #include "main.h"
 
 /**
- * string_nconcat - a function that concatenates two strings.
- *
- * @s1: first char
- * @s2: secound char
- * @n: unsigned int
- *
- * Return: If the function fails, it should return NULL
+ * _memset - copy char
+ * @s: string
+ * @b: input
+ * @n: bytes
+ * Return: string
  */
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int x, y, z;
-	char *s;
+	unsigned int i;
 
-	if (s1 == NULL)
+	for (i = 0; i < n; i++)
 	{
-		x = 0;
+		s[i] = b;
 	}
-	else
-	{
-		for (x = 0; s1[x]; ++x)
-		;
-	}
-	if (s2 == NULL)
-	{
-		y = 0;
-	}
-	else
-	{
-		for (y = 0; s2[y]; ++y)
-		;
-	}
-	if (y > n)
-		y = n;
-	s = malloc(sizeof(char) * (x + y + 1));
-	if (s == NULL)
-		return (NULL);
-	for (z = 0; z < x; z++)
-		s[z] = s1[z];
-	for (z = 0; z < y; z++)
-		s[z + x] = s2[z];
-	s[x + y] = '\0';
 	return (s);
+}
+
+/**
+ * _calloc - allocates memory for an array using malloc
+ * @nmemb: n elements
+ * @size: bytes
+ * Return: pointer
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *p;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	p = malloc(nmemb * size);
+
+	if (p == NULL)
+		return (NULL);
+	_memset(p, 0, (nmemb * size));
+	return (p);
 }
